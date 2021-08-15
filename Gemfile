418002dba76bae76661991b1a169df16999630ae
Igor Simdyanov <igorsimdyanov@gmail.com>
2021-08-15 14:57:27 +0300

    Seven lesson end

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ gem 'bootsnap', '>= 1.4.4', require: false
 gem 'slim-rails'
 
 group :development, :test do
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'ffaker'
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :name, length: { maximum: 16, minimum: 2 }
   validates :name, uniqueness: true
+  scope :default, -> { where(role_id: Role.find_by(code: :default)) }
+  scope :fresh, ->(created_at) { where('created_at > ?', created_at) }
+  scope :default_fresh, ->(created_at) { default.fresh(created_at) }
 end

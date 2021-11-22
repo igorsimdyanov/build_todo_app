0a4187933237684d693fef7204788630cdae2f92
Игорь Симдянов <igorsimdyanov@gmail.com>
2021-11-22 10:10:25 +0300

    2.8 Mailers (#7)

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,5 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: email_address_with_name('no-reply@example.com', 'TODO-сайт')
+  layout 'mailer'
+end
+
---
+++ app/mailers/notify_mailer.rb
@@ -0,0 +1,9 @@
+class NotifyMailer < ApplicationMailer
+  def user_deadlines(user, events)
+    @events = events
+    mail(
+      to: email_address_with_name(user.email, user.name),
+      subject: I18n.t('emails.subjects.user_deadlines')
+    )
+  end
+end

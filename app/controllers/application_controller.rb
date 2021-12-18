c85808b974b4bb373df7febf7bbbf0bbf305bc39
Igor Simdyanov <igorsimdyanov@gmail.com>
2021-12-18 11:30:07 +0300

    3.4 Настройка выхода и выхода в ActiveAdmin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name avatar])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name email avatar])
   end
+
+  def after_sign_in_path_for(user)
+    user.admin? ? admin_dashboard_path : root_path 
+  end
 end

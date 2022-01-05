19f2472d73b55fb914e10b7cc1061af7174b7373
Игорь Симдянов <igorsimdyanov@gmail.com>
2022-01-05 13:23:08 +0300

    III.7 Подключение redis-rails к проекту (#21)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,8 @@ Rails.application.configure do # rubocop:disable Metrics/BlockLength
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
-    config.cache_store = :memory_store
+    # config.cache_store = :memory_store
+    config.cache_store = :file_store, "#{root}/tmp/cache/views"
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }

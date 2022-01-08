bf30298096a2449906457717fef414f3169ac7e7
Igor Simdyanov <igorsimdyanov@gmail.com>
2022-01-08 23:53:03 +0300

    III.9 Таблица замыканий

---
+++ app/models/tree_comment.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: tree_comments
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  child_id   :integer
+#  parent_id  :integer
+#
+# Indexes
+#
+#  index_tree_comments_on_parent_id_and_child_id  (parent_id,child_id) UNIQUE
+#
+class TreeComment < ApplicationRecord
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -48,14 +48,40 @@ end
 Item.create! hash_items
 
 def create_event(users, event, parent_id = nil)
-  Comment.create!(
+  comment = Comment.create!(
              user: users.sample,
              content: FFaker::HipsterIpsum.paragraphs,
              commentable_id: event.id,
-             commentable_type: event.class.to_s,
-             parent_id: parent_id
+             commentable_type: event.class.to_s
            )
+  create_tree_comments(comment, parent_id)
+
+  comment
 end
+
+def create_tree_comments(comment, parent_id)
+  tree_comments = [{
+    parent_id: comment.id,
+    child_id: comment.id
+  }]
+  if parent_id
+    TreeComment.where(parent_id: parent_id).each do |tree|
+      tree_comments << {
+        parent_id: tree.parent_id,
+        child_id: comment.id
+      }
+    end
+  end
+  TreeComment.create!(tree_comments.uniq)
+end
+
+# events.each do |event|
+#   root = create_event(users, event)
+#     create_event(users, event, root.id)
+#     first = create_event(users, event, root.id)
+#       create_event(users, event, first.id)
+#     create_event(users, event, root.id)
+# end
 
 events.each do |event|
   root = create_event(users, event)

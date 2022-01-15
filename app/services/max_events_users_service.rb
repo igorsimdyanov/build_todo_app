1ccd9bbeb0c6215bea170899ab7c4a061bf78efe
Igor Simdyanov <igorsimdyanov@gmail.com>
2022-01-15 20:23:20 +0300

    III.9 Сервис-объекты

---
+++ app/services/max_events_users_service.rb
@@ -0,0 +1,20 @@
+class MaxEventsUsersService
+  attr_accessor :max_count
+
+  def initialize(max_count = 3)
+    @max_count = max_count
+  end
+
+  def call
+    User.where(id: user_ids).pluck(:email)
+  end
+
+  private
+
+  def user_ids
+    Event.group(:user_id)
+         .count
+         .max_by(max_count) { |_user_id, count| count}
+         .map { |user_id, _count| user_id }
+  end
+end

a2a2dea14ca7a974b39a67c9d3b7483c1ecca3dd
Igor Simdyanov <igorsimdyanov@gmail.com>
2022-01-19 11:57:15 +0300

    III.9 Вложенные dry-struct модели

--- app/stucts/unit.rb
+++ app/stucts/unit.rb
@@ -5,7 +5,7 @@ class Unit < Dry::Struct
   attribute? :description, Types::String
   attribute :price, Types::Coercible::Float
   attribute :currency, Currency
-  attribute :attachment do
+  attribute :attachments, Types::Array do
     attribute :name, Types::String
     attribute :url, Types::String
   end

2fb7a88ddb48db367d11d544ec2e9e546ced651f
Igor Simdyanov <igorsimdyanov@gmail.com>
2022-01-19 11:19:25 +0300

    III.9 dry-struct

--- app/stucts/unit.rb
+++ app/stucts/unit.rb
@@ -1,8 +1,12 @@
 class Unit < Dry::Struct
-  Currency = Types::String.enum('RUB', 'USD', 'EUR').default('RUB')
+  Currency = Types::String.default('RUB'.freeze).enum('RUB', 'USD', 'EUR')
 
   attribute :name, Types::String
-  attribute :description, Types::String.optional
+  attribute? :description, Types::String
   attribute :price, Types::Coercible::Float
   attribute :currency, Currency
+  attribute :attachment do
+    attribute :name, Types::String
+    attribute :url, Types::String
+  end
 end

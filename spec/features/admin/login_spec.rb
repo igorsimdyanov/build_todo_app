6ec67323dd7a35c190ec1c4c836200d5bb5a2c00
Igor Simdyanov <igorsimdyanov@gmail.com>
2022-03-04 22:04:46 +0300

    VII.9 Рефакторинг тестов

--- spec/features/admin/login_spec.rb
+++ spec/features/admin/login_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Вход в систему', driver: :selenium_chrome, js: true do
+  before :each do
+    visit new_user_session_path
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    click_button 'commit'    
+  end
+
   context 'для пользователя администратора' do
-    let(:admin_user) { create :admin_user }
+    let(:user) { create :admin_user }
 
     it 'администрирования происходит успешно' do
-      visit new_user_session_path
-      fill_in 'user_email', with: admin_user.email
-      fill_in 'user_password', with: admin_user.password
-      click_button 'commit'
-      screenshot_and_save_page
       expect(page).to have_current_path admin_dashboard_path, ignore_query: true
       expect(page).to have_content('Панель управления')
     end
@@ -19,19 +21,11 @@ RSpec.describe 'Вход в систему', driver: :selenium_chrome, js: true 
     let(:user) { create :user }
 
     it 'происходит успешно' do
-      visit new_user_session_path
-      fill_in 'user_email', with: user.email
-      fill_in 'user_password', with: user.password
-      click_button 'commit'
       expect(page).to have_no_current_path admin_dashboard_path, ignore_query: true
       expect(page).not_to have_content('Панель управления')
     end
 
     it 'администрирования завершается неудачей' do
-      visit new_user_session_path
-      fill_in 'user_email', with: user.email
-      fill_in 'user_password', with: user.password
-      click_button 'commit'
       visit admin_dashboard_path
       expect(page).to have_current_path new_user_session_path, ignore_query: true
     end
